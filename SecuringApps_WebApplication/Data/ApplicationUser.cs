670695a7f9b6369e771a5101e86cdd6af417585f
Kyle Buttigieg <Kyle.Buttigieg.c10242@mcast.edu.mt>
2021-03-04 12:41:59 +0100

    Add project files.

---
+++ SecuringApps_WebApplication/Data/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecuringApps_WebApplication.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+
+
+        [Required]
+        [PersonalData]
+        public String Address { get; set; }
+
+
+
+    }
+}
